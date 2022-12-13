07b26a068a4a1734151a2999434a4cc62b77fe6f
Paulo Vareiro <58229468+paulovareiro29@users.noreply.github.com>
2022-12-13 12:46:10 +0000

    db-rel schema

--- images/db/rel/initial_schema.sql
+++ images/db/rel/initial_schema.sql
@@ -2,39 +2,54 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 CREATE EXTENSION IF NOT EXISTS POSTGIS;
 CREATE EXTENSION IF NOT EXISTS POSTGIS_TOPOLOGY;
 
-CREATE TABLE public.teams (
-	id              uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
-	name            VARCHAR(250) NOT NULL,
+CREATE TABLE public.areas (
+	id				uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+	name			VARCHAR(250) NOT NULL,
 	created_on      TIMESTAMP NOT NULL DEFAULT NOW(),
 	updated_on      TIMESTAMP NOT NULL DEFAULT NOW()
 );
 
-CREATE TABLE public.countries (
-	id              uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
-	name            VARCHAR(250) UNIQUE NOT NULL,
+CREATE TABLE public.types (
+	id				uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+	name			VARCHAR(250) NOT NULL,
 	created_on      TIMESTAMP NOT NULL DEFAULT NOW(),
 	updated_on      TIMESTAMP NOT NULL DEFAULT NOW()
 );
 
-CREATE TABLE public.players (
-	id              uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
-	name            VARCHAR(250) NOT NULL,
-	age             INT NOT NULL,
-	team_id         uuid,
-	country_id      uuid NOT NULL,
+
+CREATE TABLE public.hosts (
+	id				uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+	name			VARCHAR(250) NOT NULL,
+	verified		VARCHAR(250) NOT NULL,
 	created_on      TIMESTAMP NOT NULL DEFAULT NOW(),
 	updated_on      TIMESTAMP NOT NULL DEFAULT NOW()
 );
 
-ALTER TABLE players
-    ADD CONSTRAINT players_countries_id_fk
-        FOREIGN KEY (country_id) REFERENCES countries
+CREATE TABLE public.airbnbs (
+	id				uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+	name			VARCHAR(250) NOT NULL,
+	price			INT,
+	host_id			uuid NOT NULL,
+	type_id			uuid,
+	area_id			uuid,
+	neighbourhood	VARCHAR(250),
+	geom			GEOMETRY,
+	created_on      TIMESTAMP NOT NULL DEFAULT NOW(),
+	updated_on      TIMESTAMP NOT NULL DEFAULT NOW()
+);
+
+ALTER TABLE airbnbs
+    ADD CONSTRAINT airbnbs_hosts_id_fk
+        FOREIGN KEY (host_id) REFERENCES hosts
             ON DELETE CASCADE;
 
-ALTER TABLE players
-    ADD CONSTRAINT players_teams_id_fk
-        FOREIGN KEY (team_id) REFERENCES teams
+ALTER TABLE airbnbs
+    ADD CONSTRAINT airbnbs_type_id_fk
+        FOREIGN KEY (type_id) REFERENCES types
             ON DELETE SET NULL;
 
-
+ALTER TABLE airbnbs
+    ADD CONSTRAINT airbnbs_areas_id_fk
+        FOREIGN KEY (area_id) REFERENCES areas
+            ON DELETE SET NULL;
 
--- src/importer/entities/area.py
+++ src/importer/entities/area.py
@@ -7,15 +7,11 @@ class Area:
         Area.counter += 1
         self._id = Area.counter
         self._name = name
-        self._lat = 0
-        self._lon = 0
 
     def to_xml(self):
         el = XMLElement(tag="Area", attrib={
             "id": str(self._id),
-            "name": self._name,
-            "lat": str(self._lat),
-            "lon": str(self._lon)
+            "name": self._name
         })
         return el
 
@@ -23,7 +19,7 @@ class Area:
         return self._id
 
     def __str__(self):
-        return f"id:{self._id}, name: {self._name}, lat: {self._lat}, lon: {self._lon}"
+        return f"id:{self._id}, name: {self._name}"
 
 
 Area.counter = 0
