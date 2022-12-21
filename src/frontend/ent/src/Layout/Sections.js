70c6937a3886dadf1a8b39436c3e365e50444929
Paulo Vareiro <58229468+paulovareiro29@users.noreply.github.com>
2022-12-21 20:11:58 +0000

    added hosts to frontend-ent

--- src/frontend/ent/src/Layout/Sections.js
+++ src/frontend/ent/src/Layout/Sections.js
@@ -1,25 +1,23 @@
-import Players from "../Tables/Players";
+import Hosts from "../Tables/Hosts";
 
 const Sections = [
+  {
+    id: "hosts",
+    label: "Hosts",
+    content: <Hosts />,
+  },
 
-    {
-        id: "players",
-        label: "Players",
-        content: <Players/>
-    },
+  {
+    id: "teams",
+    label: "Teams",
+    content: <h1>Teams - Work in progresss</h1>,
+  },
 
-    {
-        id: "teams",
-        label: "Teams",
-        content: <h1>Teams - Work in progresss</h1>
-    },
-
-    {
-        id: "countries",
-        label: "Countries",
-        content: <h1>Countries - Work in progress</h1>
-    }
-
+  {
+    id: "countries",
+    label: "Countries",
+    content: <h1>Countries - Work in progress</h1>,
+  },
 ];
 
 export default Sections;
---
+++ src/frontend/ent/src/Tables/Hosts.js
@@ -0,0 +1,102 @@
+import { useEffect, useState } from "react";
+import {
+  CircularProgress,
+  Pagination,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+} from "@mui/material";
+
+import useAPI from "../Hooks/useAPI";
+
+function Hosts() {
+  const PAGE_SIZE = 10;
+  const { GET } = useAPI();
+  const [page, setPage] = useState(1);
+  const [data, setData] = useState(null);
+  const [maxDataSize, setMaxDataSize] = useState(0);
+
+  useEffect(() => {
+    GET(`/host/?page=${page - 1}&limit=${PAGE_SIZE}`)
+      .then((result) => {
+        const data = result.data;
+        setData(data.data);
+        setMaxDataSize(data.pagination.count);
+      })
+      .catch((err) => console.log(err));
+  }, [page]);
+
+  if (!data)
+    return (
+      <>
+        <h1>Loading..</h1>
+      </>
+    );
+
+  return (
+    <>
+      <h1>Hosts</h1>
+
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell component="th" width={"1px"} align="center">
+                ID
+              </TableCell>
+              <TableCell>Host Name</TableCell>
+              <TableCell align="center">Verified Status</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {data ? (
+              data.map((row) => (
+                <TableRow
+                  key={row.id}
+                  style={{ background: "gray", color: "black" }}
+                >
+                  <TableCell component="td" align="center">
+                    {row.id}
+                  </TableCell>
+                  <TableCell component="td" scope="row">
+                    {row.name}
+                  </TableCell>
+                  <TableCell component="td" align="center" scope="row">
+                    {row.verified}
+                  </TableCell>
+                </TableRow>
+              ))
+            ) : (
+              <TableRow>
+                <TableCell colSpan={3}>
+                  <CircularProgress />
+                </TableCell>
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      {maxDataSize && (
+        <div style={{ background: "black", padding: "1rem" }}>
+          <Pagination
+            style={{ color: "black" }}
+            variant="outlined"
+            shape="rounded"
+            color={"primary"}
+            onChange={(e, v) => {
+              setPage(v);
+            }}
+            page={page}
+            count={Math.ceil(maxDataSize / PAGE_SIZE)}
+          />
+        </div>
+      )}
+    </>
+  );
+}
+
+export default Hosts;
