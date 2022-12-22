c6b4a6098479efb5ccfea2ec049af5e21a0fccfd
Paulo Vareiro <58229468+paulovareiro29@users.noreply.github.com>
2022-12-22 20:08:58 +0000

    removed demo from frontend-gis

--- src/frontend/gis/src/Map/ObjectMarkersGroup.js
+++ src/frontend/gis/src/Map/ObjectMarkersGroup.js
@@ -3,61 +3,11 @@ import { LayerGroup, useMap } from "react-leaflet";
 import { ObjectMarker } from "./ObjectMarker";
 import useAPI from "../Hooks/useAPI";
 
-const DEMO_DATA = [
-  {
-    type: "feature",
-    geometry: {
-      type: "Point",
-      coordinates: [41.69462, -8.84679],
-    },
-    properties: {
-      id: "7674fe6a-6c8d-47b3-9a1f-18637771e23b",
-      name: "Ronaldo",
-      country: "Portugal",
-      position: "Striker",
-      imgUrl: "https://cdn-icons-png.flaticon.com/512/805/805401.png",
-      number: 7,
-    },
-  },
-
-  {
-    type: "feature",
-    geometry: {
-      type: "Point",
-      coordinates: [41.69662, -8.84979],
-    },
-    properties: {
-      id: "36ee2d0f-a918-472a-8e2e-ad5f567cdb89",
-      name: "Messi",
-      country: "Argentina",
-      position: "Forward",
-      imgUrl: "https://cdn-icons-png.flaticon.com/512/805/805404.png",
-      number: 10,
-    },
-  },
-
-  {
-    type: "feature",
-    geometry: {
-      type: "Point",
-      coordinates: [41.69562, -8.84979],
-    },
-    properties: {
-      id: "4cb5b2f0-343d-4250-ba5c-3a235343cb01",
-      name: "Ibrahimovic",
-      country: "Sweden",
-      position: "Striker",
-      imgUrl: "https://cdn-icons-png.flaticon.com/512/805/805409.png",
-      number: 11,
-    },
-  },
-];
-
 function ObjectMarkersGroup() {
   const map = useMap();
   const { GET } = useAPI();
 
-  const [geom, setGeom] = useState([...DEMO_DATA]);
+  const [geom, setGeom] = useState([]);
   const [bounds, setBounds] = useState(map.getBounds());
 
   /**
