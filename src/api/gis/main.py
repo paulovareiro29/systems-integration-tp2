13b0f3076f5facdcda4283b63b8128ef4a165bad
Paulo Vareiro <58229468+paulovareiro29@users.noreply.github.com>
2022-12-22 19:10:30 +0000

    added api-gis - tile

--- src/api/gis/main.py
+++ src/api/gis/main.py
@@ -1,14 +1,18 @@
 import sys
 
-from flask import Flask, request
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+
+from utils.database import Database
 
 PORT = int(sys.argv[1]) if len(sys.argv) >= 2 else 9000
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
 
+CORS(app)
 
-@app.route('/api/markers', methods=['GET'])
+''' @app.route('/api/markers', methods=['GET'])
 def get_markers():
     args = request.args
 
@@ -28,7 +32,41 @@ def get_markers():
                 "number": 7
             }
         }
-    ]
+    ] '''
+
+
+@app.route('/api/tile')
+def get_markers():
+    args = request.args
+
+    try:
+        neLat = args["neLat"]
+        neLng = args["neLng"]
+        swLat = args["swLat"]
+        swLng = args["swLng"]
+    except:
+        return jsonify("Invalid arguments"), 400
+
+    try:
+        db = Database()
+
+        markers = db.selectAll(
+            """SELECT jsonb_build_object(
+                    'type', 'Feature',
+                    'id', id,
+                    'geometry', ST_AsGeoJSON(geom):: jsonb,
+                    'properties', to_jsonb(t.*) - 'id' - 'geom'
+                ) AS json
+                    FROM (SELECT id, name, geom FROM airbnbs WHERE geom && ST_MakeEnvelope(%s,%s,%s,%s)) AS t(id, name, geom)""", (neLat, neLng, swLat, swLng))
+
+        res = []
+        for m in markers:
+            res.append(m)
+
+        return jsonify(res)
+    except Exception as err:
+        print(err)
+        return jsonify(), 400
 
 
 if __name__ == '__main__':
